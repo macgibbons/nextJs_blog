699f11986449362c7b23f299bb0e3647a198b8ce
Mac Gibbons <macgibbons@gmail.com>
2021-03-15 16:20:25 -0500

    dynamic routes

---
+++ pages/index.js
@@ -0,0 +1,62 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
+
+async function getCatFact() {
+    const res = await fetch("https://catfact.ninja/facts?limit=1&max_length=140");
+    return res.json();
+}
+
+export async function getStaticProps() {
+    const catData = await getCatFact();
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData,
+            catData,
+        },
+    };
+}
+
+export default function Home({ allPostsData, catData }) {
+    const catFact = catData.data[0];
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>This is an introduction</p>
+                <p>
+                    (This is a sample website - you’ll be building a site like this on{" "}
+                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+                </p>
+            </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Cat Fact</h2>
+                <ul className={utilStyles.list}>
+                    <div>{catFact.fact}</div>
+                </ul>
+            </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br />
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    );
+}
